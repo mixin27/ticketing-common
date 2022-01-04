e9b5f383593bb3b4041a6371e87760023ec3be39
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-01-04 19:34:21 +0630

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 100,
+  green: 100,
+  blue: 100,
+};
+
+console.log(color);
+
+export default color;
