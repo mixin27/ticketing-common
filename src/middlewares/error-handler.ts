a8145ad447f44aae364e6262d17ef78a8bf0c5f6
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-01-06 16:01:54 +0630

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -12,6 +12,8 @@ export const errorHandler = async (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error(err);
+
   res.status(400).send({
     errors: [{ message: 'Something went wrong' }],
   });
