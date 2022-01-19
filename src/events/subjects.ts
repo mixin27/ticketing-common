4e08bf7b85911b504bb25cd0936108688b1ef76c
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-01-19 20:57:29 +0630

    Updates

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -1,4 +1,8 @@
 export enum Subjects {
+  // Ticket
   TicketCreated = 'ticket:created',
   TicketUpdated = 'ticket:updated',
+  // Order
+  OrderCreated = 'order:created',
+  OrderCancelled = 'order:cancelled',
 }
--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,12 @@ export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
 
-// events
+// events and event types
 export * from './events/base-publisher';
 export * from './events/base-listener';
 export * from './events/subjects';
+export * from './events/types/order-status';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
-
-// types
-export * from './events/types/order-status';
+export * from './events/order-created-event';
+export * from './events/order-cancelled-event';
